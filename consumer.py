5e55d138dbf6a1348a15bf1cb2bd28dd5a71a596
Boyu1997 <jiangby97@gmail.com>
2020-12-18 22:04:53 -0500

    Simple Kafka consumer

---
+++ consumer.py
@@ -0,0 +1,31 @@
+from confluent_kafka import Consumer, KafkaError
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9091',
+    'group.id': 'counting-group',
+    'client.id': 'client-1',
+    'enable.auto.commit': True,
+    'session.timeout.ms': 6000,
+    'default.topic.config': {'auto.offset.reset': 'smallest'}
+})
+
+c.subscribe(['light_bulb'])
+
+try:
+    while True:
+        msg = c.poll(0.1)
+        if msg is None:
+            continue
+        elif not msg.error():
+            print('Received message: {0}'.format(msg.value()))
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            print('End of partition reached {0}/{1}'
+                  .format(msg.topic(), msg.partition()))
+        else:
+            print('Error occured: {0}'.format(msg.error().str()))
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    c.close()
